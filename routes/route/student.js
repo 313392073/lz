fd1519f92b6dfb8e4beb7aa77b1671bbdb101119
x xiao <x xiao>
2018-12-15 00:04:21 +0800

    视频详情

--- routes/route/student.js
+++ routes/route/student.js
@@ -5,4 +5,17 @@ module.exports = function(router){
             title:'正棱锥侧面积'
         })
     })
+     // 视频详情
+     router.get('/videdetail.html',function(req,res){
+        res.render('videDetail',{
+            title:'视频详情'
+        })
+    })
+
+     // 任务详情
+     router.get('/taskdetail.html',function(req,res){
+        res.render('taskDetail',{
+            title:'任务详情'
+        })
+    })
 }
