0af5d6aa0f1d6f69f7b50a662ac83384aa91fda9
x xiao <x xiao>
2018-12-16 14:33:57 +0800

    提交

--- routes/route/student.js
+++ routes/route/student.js
@@ -5,6 +5,15 @@ module.exports = function(router){
             title:'正棱锥侧面积'
         })
     })
+
+     // 正棱锥表面积
+     router.get('/regularareaformula.html',function(req,res){
+        res.render('RegularAreaFormula',{
+            title:'正棱锥侧面积'
+        })
+    })
+
+
      // 视频详情
      router.get('/videdetail.html',function(req,res){
         res.render('videDetail',{
@@ -16,6 +25,56 @@ module.exports = function(router){
      router.get('/taskdetail.html',function(req,res){
         res.render('taskDetail',{
             title:'任务详情'
+        })
+    })
+
+    // 线线关系
+    router.get('/linerelation.html',function(req,res){
+        res.render('lineRelation',{
+            title:'线线关系'
+        })
+    })
+
+    // 课后实验
+    router.get('/aftclaexp.html',function(req,res){
+        res.render('aftClaExp',{
+            title:'课后实验'
+        })
+    })
+
+     // 视频分享
+     router.get('/videoshare.html',function(req,res){
+        res.render('videoShare',{
+            title:'视频分享'
+        })
+    })
+
+    // 寻找棱锥
+    router.get('/findpyramid.html',function(req,res){
+        res.render('findPyramid',{
+            title:'寻找棱锥'
+        })
+    })
+
+
+    // 制作棱锥
+    router.get('/makepyramid.html',function(req,res){
+        res.render('makePyramid',{
+            title:'制作棱锥'
+        })
+    })
+
+    // 课后习题
+    router.get('/homeworkproblems.html',function(req,res){
+        res.render('HomeworkProblems',{
+            title:'课后习题'
+        })
+    })
+
+    // 课件管理
+    router.get('/coursemanagement.html',function(req,res){
+        res.render('courseManagement',{
+            title:'课件管理'
         })
     })
 }
